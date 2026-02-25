580f33c5db4e2e66784938e01c0673d44a85b534
Jarl Lyng <jarllyng@gmail.com>
2026-02-25 13:50:21 +0100

    Initial Commit

---
+++ echolume/echolumeApp.swift
@@ -0,0 +1,17 @@
+//
+//  echolumeApp.swift
+//  echolume
+//
+//  Created by Jarl Lyng on 25/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct echolumeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ echolumeTests/echolumeTests.swift
@@ -0,0 +1,17 @@
+//
+//  echolumeTests.swift
+//  echolumeTests
+//
+//  Created by Jarl Lyng on 25/02/2026.
+//
+
+import Testing
+@testable import echolume
+
+struct echolumeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
